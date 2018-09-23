0d89b8bd7ccc91871337f1ad5769f8e2378760cc
Mario Galluscio <mgalluscio@deliverydudes.com>
2018-09-23 17:02:26 -0400

    "Back button"

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -15,11 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) { }
 
 
 }
